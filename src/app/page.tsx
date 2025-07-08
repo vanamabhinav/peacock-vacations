ffa14095e97538a0f676a5151e274d9dc755554c
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2025-07-08 05:41:51 +0530

    feat: add VisaMadeEasy component for streamlined visa assistance section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import PopularDestinations from "@/sections/PopularDestinations";
 import PopularPackages from "@/sections/PopularPackage";
+import VisaMadeEasy from "@/sections/VisaMadeEasy";
 
 export default function Home() {
   return (
     <div>
       <PopularDestinations />
       <PopularPackages />
+      <VisaMadeEasy />
     </div>
   );
 }
