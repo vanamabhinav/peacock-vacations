a4a8192164c0404ac7e83d87f54899b5e559b658
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2025-07-16 09:58:08 +0530

    feat: restructure homepage sections and migrate to new directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import HeroSectionCarousel from "@/components/slider/HeroSectionCarousel";
 import { fetchHomePageCmsDataAction } from "@/lib/actions/fetchHomePageCmsAction";
-import PopularDestinations from "@/sections/PopularDestinations";
-import PopularPackages from "@/sections/PopularPackage";
-import TravelByTheme from "@/sections/TravelByTheme";
-import VisaMadeEasy from "@/sections/VisaMadeEasy";
+import PopularDestinations from "@/sections/homepage/PopularDestinations";
+import PopularPackages from "@/sections/homepage/PopularPackage";
+import TravelByTheme from "@/sections/homepage/TravelByTheme";
+import VisaMadeEasy from "@/sections/homepage/VisaMadeEasy";
 
 export default async function Home() {
   const homePageData = await fetchHomePageCmsDataAction();
   return (
-    <div>
+    <main role="main">
       <HeroSectionCarousel {...homePageData.heroSectionData} />
       <PopularDestinations {...homePageData.popularDestinationsSectionData} />
       <VisaMadeEasy {...homePageData.visaAssistanceSectionData} />
       <PopularPackages {...homePageData.popularPackagesSectionData} />
       <TravelByTheme {...homePageData.travelByThemeSectionData} />
-    </div>
+    </main>
   );
 }
